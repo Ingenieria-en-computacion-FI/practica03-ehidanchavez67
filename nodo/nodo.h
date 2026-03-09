febc70cb728c95cca7f80558c51f7272f391fed1
ehidanchavez67 <ehidanchavez@unam.edu>
2026-03-08 20:40:38 -0600

    Update nodo.h

--- nodo/nodo.h
+++ nodo/nodo.h
@@ -4,17 +4,16 @@
 /* Declaración incompleta del tipo Nodo (tipo opaco) */
 typedef struct Nodo Nodo;
 
-
 /* Crea un nodo con el valor indicado */
 Nodo* crearNodo(int valor);
 
 /* Obtiene el valor almacenado en el nodo */
-int obtenerValor(Nodo* nodo);
+int obtenerValor(Nodo* n);
 
 /* Cambia el valor almacenado en el nodo */
-void asignarValor(Nodo* nodo, int valor);
+void asignarValor(Nodo* n, int nuevoValor);
 
 /* Libera la memoria del nodo */
-void destruirNodo(Nodo* nodo);
+void destruirNodo(Nodo* n);
 
 #endif
